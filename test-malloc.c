bec2292cd874849d55ba2ea1f81dfc89ca5eabfe
sartaj <sartajs2002@gmail.com>
2023-12-08 22:31:24 -0500

    calloc working

--- test-malloc.c
+++ test-malloc.c
@@ -4,17 +4,5 @@
 
 int main(int argc, char *argv[])
 {
-	char *ptr = malloc(8);
-
-	*ptr = 'a';
-	*(ptr + 1) = 'b';
-
-	char *ptr2 = realloc(ptr, 12);
-	*(ptr2 + 2) = 'c';
-	for (int i = 0; i < 3; i++) {
-		printf("%c ", *(ptr2 + i));
-	}
-
-	getchar();
 	return 0;
 }
